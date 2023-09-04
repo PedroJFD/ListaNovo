4879ad81642ca86e5594102cd426642c443c6c0d
Pedro Joaquim <pedro.joaquim@estudante.ifro.edu.br>
2023-09-04 14:01:27 -0400

    Lista

---
+++ RevisaoGeralReal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> list = new List<string>();
+        list.Add("a");
+        list.Insert(0, "b");//tem q indicar qual posição
+
+        List<int> listint = new List<int>();
+        listint.Add(1);
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
